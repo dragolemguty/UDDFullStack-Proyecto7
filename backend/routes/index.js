b3ae2de7d4f731cd239f407d35c58124e9cb3718
dragolemguty <juan.jose.gutierrez.terraza@gmail.com>
2024-10-14 20:45:42 -0300

    fix profile,mis reservas

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,34 @@ const express = require("express");
 const router = express.Router();
 const { swaggerDocs, swaggerUi } = require("../swaggerConfig");
 const auth2 = require('../middleware/authorization3');
+const Booking = require('../models/booking');
+
+
+
+router.get('/reservas/user-bookings', auth2, async (req, res) => {
+    try {
+      const userId = req.user._id;
+      // Buscar todas las reservas asociadas al usuario
+      const bookings = await Booking.find({ id_guest: userId }).populate('id_room_array');
+  
+      // Enviar respuesta
+      return res.status(200).json(bookings);
+    } catch (error) {
+      return res.status(500).json({ message: 'Error al obtener las reservas del usuario', error });
+    }
+  });
+
+
+  router.get('/users/me', auth2, async (req, res) => {
+    try {
+        if (!req.user) {
+            return res.status(404).send({ error: 'User not found' });
+        }
+        res.send(req.user);  // Asegúrate de que req.user contiene datos
+    } catch (error) {
+        res.status(500).send({ error: 'Error al obtener el perfil del usuario' });
+    }
+});
 
 
 const bookingRouter = require("./booking.routes");
@@ -15,17 +43,6 @@ const payRoutes = require('./payment.routes'); //
 const cartRoutes = require('./cart.routes'); // Importamos las rutas de carrito
 
 
-router.get('/users/me', auth2, async (req, res) => {
-    try {
-        if (!req.user) {
-            return res.status(404).send({ error: 'User not found' });
-        }
-        res.send(req.user);  // Asegúrate de que req.user contiene datos
-    } catch (error) {
-        res.status(500).send({ error: 'Error al obtener el perfil del usuario' });
-    }
-});
-
 router.use("/reservas", bookingRouter); // Rutas para reservas
 router.use("/eventos", guestEventRouter); // Rutas para eventos de huésped
 router.use("/rooms", roomRouter); // Rutas para habitaciones
@@ -35,6 +52,7 @@ router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/cart", cartRoutes); // Añadimos las rutas de carrito
 router.use("/pay", payRoutes); // Añadimos las rutas de carrito
+
 
 // Ruta para la documentación de Swagger
 router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
